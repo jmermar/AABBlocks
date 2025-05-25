33a9838276f16bd1497cad48342611b90d04e3ef
jmeronom <josesmerono@gmail.com>
2025-05-25 04:03:09 +0200

    Created RAII class

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,45 @@
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include "types.hpp"
 #include <glm/glm.hpp>
+#include "vk/vk_raii.hpp"
+using namespace vblck;
+
 std::shared_ptr<spdlog::logger> logger;
 
-SDL_Window* window = 0;
+SDL_Window *window = 0;
 
+namespace vblck
+{
 
-namespace vblck {
-
-    std::shared_ptr<spdlog::logger>& getLogger() {
+    std::shared_ptr<spdlog::logger> &getLogger()
+    {
         return logger;
-    }   
+    }
 }
 
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     logger = spdlog::stdout_color_mt("VKP");
 
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
     LOG_INFO("SDL Initialized");
 
     TRY(window = SDL_CreateWindow("AABBLOCKS", 1280, 720, SDL_WINDOW_VULKAN));
-    
+
     LOG_INFO("Window created");
 
     bool running = true;
 
-    while (running) {
+    while (running)
+    {
         SDL_Event e;
-        while(SDL_PollEvent(&e)) {
-            switch (e.type) {
-                case SDL_EVENT_QUIT:
-                    running = false;
-                    break;
+        while (SDL_PollEvent(&e))
+        {
+            switch (e.type)
+            {
+            case SDL_EVENT_QUIT:
+                running = false;
+                break;
             }
         }
     }
---
+++ src/vk/vk_raii.hpp
@@ -0,0 +1,85 @@
+#include "types.hpp"
+#include <vulkan/vulkan.h>
+namespace vblck
+{
+    namespace vk
+    {
+        template <typename Raw, void (*Des)(VkDevice, Raw)>
+        class RAII
+        {
+        public:
+            RAII()
+                : raw(nullptr)
+            {
+            }
+
+            RAII(VkDevice &&device, Raw &&raw)
+                : raw(raw), device(device)
+            {
+            }
+
+            ~RAII()
+            {
+                Destroy();
+            }
+
+            void set(VkDevice &&device, Raw &&raw)
+            {
+                Destroy();
+                this->device = device;
+                this->raw = raw;
+            }
+
+            RAII &operator=(const RAII &other) = delete;
+
+            RAII(const RAII &other) = delete;
+
+            RAII &operator=(RAII &&other)
+            {
+                Destroy();
+                assert(raw == nullptr);
+                raw = other.raw;
+                device = other.device;
+                other.raw = nullptr;
+                other.device = nullptr;
+                return *this;
+            }
+
+            RAII(RAII &&other)
+                : raw(other.raw)
+            {
+                other.raw = nullptr;
+            }
+
+            operator bool() const { return raw; }
+            const Raw &operator*() const { return raw; }
+            Raw &operator*() { return raw; }
+            const Raw *operator->() const { return &raw; }
+            Raw *operator->() { return &raw; }
+
+        private:
+            void Destroy()
+            {
+                if (!raw)
+                    return;
+                Des(raw);
+                raw = nullptr;
+            }
+
+        private:
+            Raw raw;
+            VkDevice device;
+        };
+
+#define MAKE_RAII(vulkanType, delFun) using RAII_##vulkanType = RAII<vulkanType, [](VkDevice d, vulkanType t) { delFun(d, t, nullptr); }>;
+
+        MAKE_RAII(VkBuffer, vkDestroyBuffer);
+        MAKE_RAII(VkImage, vkDestroyImage);
+        MAKE_RAII(VkFence, vkDestroyFence);
+        MAKE_RAII(VkSemaphore, vkDestroySemaphore);
+        MAKE_RAII(VkImageView, vkDestroyImageView);
+        MAKE_RAII(VkPipeline, vkDestroyPipeline);
+        MAKE_RAII(VkPipelineLayout, vkDestroyPipelineLayout);
+#undef MAKE_RAII;
+    }
+}
